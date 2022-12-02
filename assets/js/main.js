3e418d7101362543be59fc9b134dd35cdcb41a72
Angel Crawford <16148683+AngelCrawford@users.noreply.github.com>
2022-12-02 01:06:28 +0100

    Working on the header title, #137

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,8 +14,10 @@ $(".navbar-item.has-dropdown").click(function () {
   $(this).children('.navbar-dropdown').toggle();
 });
 
+
 if ($(window).width() <= 1023) {
   $(".navbar-item.has-dropdown a.navbar-link").removeAttr("href");
+  $('.navbar-item.has-dropdown .navbar-dropdown').css("display", "none");
 }
 
 // ***************** Back to Top Button
@@ -54,259 +56,259 @@ $("span.shimmer").mouseenter(function() {
   }); 
 });
 
-// Call every hour, or every page reload
-setInterval(dayNightSky(), 60*60*1000);
+// // Call every hour, or every page reload
+// setInterval(dayNightSky(), 60*60*1000);
 
 
-// ***************** Sticky Navbar
-// $(window).scroll(function () {
-//   navScroll();
+// // ***************** Sticky Navbar
+// // $(window).scroll(function () {
+// //   navScroll();
+// // });
+
+// // navScroll();
+
+
+// // ***************** Spoiler
+// $('.spoiler').click(function () {
+//   $(this).toggleClass('visible');
 // });
 
-// navScroll();
+// // ***************** Comments
+// var invalidClassName = 'is-danger'
+// var inputs = document.querySelectorAll('input, select, textarea')
+// inputs.forEach(function (input) {
+//   // Add a css class on submit when the input is invalid.
+//   input.addEventListener('invalid', function () {
+//     input.classList.add(invalidClassName);
 
+//     $('#' + input.id).parent().next('.help.is-danger').css('display', 'block');
+//     $('#' + input.id).parent().children('.icon.is-right.is-danger').css('display', 'inline-flex');
+//     $('#' + input.id).parent().addClass('is-danger');
+//   })
 
-// ***************** Spoiler
-$('.spoiler').click(function () {
-  $(this).toggleClass('visible');
-});
-
-// ***************** Comments
-var invalidClassName = 'is-danger'
-var inputs = document.querySelectorAll('input, select, textarea')
-inputs.forEach(function (input) {
-  // Add a css class on submit when the input is invalid.
-  input.addEventListener('invalid', function () {
-    input.classList.add(invalidClassName);
-
-    $('#' + input.id).parent().next('.help.is-danger').css('display', 'block');
-    $('#' + input.id).parent().children('.icon.is-right.is-danger').css('display', 'inline-flex');
-    $('#' + input.id).parent().addClass('is-danger');
-  })
-
-  // Remove the class when the input becomes valid.
-  // 'input' will fire each time the user types
-  input.addEventListener('input', function () {
-    if (input.validity.valid) {
-      input.classList.remove(invalidClassName);
+//   // Remove the class when the input becomes valid.
+//   // 'input' will fire each time the user types
+//   input.addEventListener('input', function () {
+//     if (input.validity.valid) {
+//       input.classList.remove(invalidClassName);
       
-      $('#' + input.id).parent().next('.help.is-danger').css('display', 'none');
-      $('#' + input.id).parent().children('.icon.is-right.is-danger').css('display', 'none');
-      $('#' + input.id).parent().removeClass('is-danger');
-    }
-  })
-});
+//       $('#' + input.id).parent().next('.help.is-danger').css('display', 'none');
+//       $('#' + input.id).parent().children('.icon.is-right.is-danger').css('display', 'none');
+//       $('#' + input.id).parent().removeClass('is-danger');
+//     }
+//   })
+// });
 
-$('#comment-form').submit(function() {
-    $('#comment-send').addClass("is-loading");
-    $('#comment-name').addClass('is-disabled');
-    $('#comment-email').addClass('is-disabled');
-    $('#comment-website').addClass('is-disabled');
-    $('#comment-message').addClass('is-disabled');
-    $('.b-checkbox').addClass('is-disabled');      
-});
+// $('#comment-form').submit(function() {
+//     $('#comment-send').addClass("is-loading");
+//     $('#comment-name').addClass('is-disabled');
+//     $('#comment-email').addClass('is-disabled');
+//     $('#comment-website').addClass('is-disabled');
+//     $('#comment-message').addClass('is-disabled');
+//     $('.b-checkbox').addClass('is-disabled');      
+// });
 
-if (window.location.hash == "#comment-submitted") {
-  $('#comment-submitted').css('display', 'block');
-}
-if (window.location.hash == "#comment-error") {
-  $('#comment-error').css('display', 'block');
-}
+// if (window.location.hash == "#comment-submitted") {
+//   $('#comment-submitted').css('display', 'block');
+// }
+// if (window.location.hash == "#comment-error") {
+//   $('#comment-error').css('display', 'block');
+// }
 
-$('#comment-submitted button.delete').click(function () {
-  $('#comment-submitted').css('display', 'none');
-  window.location.hash = "#postcomment";
-});
+// $('#comment-submitted button.delete').click(function () {
+//   $('#comment-submitted').css('display', 'none');
+//   window.location.hash = "#postcomment";
+// });
 
-$('#comment-error button.delete').click(function () {
-  $('#comment-error').css('display', 'none');
-  window.location.hash = "#postcomment";
-});
+// $('#comment-error button.delete').click(function () {
+//   $('#comment-error').css('display', 'none');
+//   window.location.hash = "#postcomment";
+// });
   
 
-// ***************** Navigation Scroll
-function navScroll() {
-  var theNavigation = $(".navbar");
-  stuck = "is-fixed-top";
-  theHeader = $('.header').height() - 56;
+// // ***************** Navigation Scroll
+// function navScroll() {
+//   var theNavigation = $(".navbar");
+//   stuck = "is-fixed-top";
+//   theHeader = $('.header').height() - 56;
 
-  if ($(this).scrollTop() > theHeader) {
-    theNavigation.addClass(stuck);
-    $(".navbar-logo").css('display', 'flex');
-  } else {
-    theNavigation.removeClass(stuck);
-    $(".navbar-logo").css('display', 'none');
-  }
+//   if ($(this).scrollTop() > theHeader) {
+//     theNavigation.addClass(stuck);
+//     $(".navbar-logo").css('display', 'flex');
+//   } else {
+//     theNavigation.removeClass(stuck);
+//     $(".navbar-logo").css('display', 'none');
+//   }
 
-  // $(".navbar-logo").css('display', 'flex');
-}
-
-
-// ***************** Comment ReplyTo Button function
-// Added function to change value onclick
-function changeValue(elementName, newValue) {
-  document.getElementsByName(elementName)[0].value=newValue;
-  window.location.hash = "#postcomment";
-};
+//   // $(".navbar-logo").css('display', 'flex');
+// }
 
 
-// ***************** Sky Background Function
-// THANKS: https://codepen.io/ellimccale/pen/wxzJMx
-function dayNightSky() {
+// // ***************** Comment ReplyTo Button function
+// // Added function to change value onclick
+// function changeValue(elementName, newValue) {
+//   document.getElementsByName(elementName)[0].value=newValue;
+//   window.location.hash = "#postcomment";
+// };
+
+
+// // ***************** Sky Background Function
+// // THANKS: https://codepen.io/ellimccale/pen/wxzJMx
+// function dayNightSky() {
   
-  var $sky = $("header.sky");
+//   var $sky = $("header.sky");
 
-  var location = {lat:"53.551086", long:"9.993682"};
-  var now = new Date();
-  var times = SunCalc.getTimes(now, location.lat, location.long);
-  // console.log(SunCalc.getTimes(now, location.lat, location.long));
+//   var location = {lat:"53.551086", long:"9.993682"};
+//   var now = new Date();
+//   var times = SunCalc.getTimes(now, location.lat, location.long);
+//   // console.log(SunCalc.getTimes(now, location.lat, location.long));
 
-  var nightStart = [ times.night.getHours(), times.night.getMinutes() + 1 ];
-  var nightEnd = [ times.nightEnd.getHours(), times.nightEnd.getMinutes() ];
+//   var nightStart = [ times.night.getHours(), times.night.getMinutes() + 1 ];
+//   var nightEnd = [ times.nightEnd.getHours(), times.nightEnd.getMinutes() ];
 
-  var dawnStart = [ times.nightEnd.getHours(), times.nightEnd.getMinutes() + 1 ];
-  var dawnEnd = [ times.sunrise.getHours(), times.sunrise.getMinutes() ];
+//   var dawnStart = [ times.nightEnd.getHours(), times.nightEnd.getMinutes() + 1 ];
+//   var dawnEnd = [ times.sunrise.getHours(), times.sunrise.getMinutes() ];
 
-  var dayStart = [ times.sunrise.getHours(), times.sunrise.getMinutes() + 1 ];
-  var dayEnd = [ times.sunset.getHours(), times.sunset.getMinutes() ];
+//   var dayStart = [ times.sunrise.getHours(), times.sunrise.getMinutes() + 1 ];
+//   var dayEnd = [ times.sunset.getHours(), times.sunset.getMinutes() ];
 
-  var duskStart = [ times.sunset.getHours(), times.sunset.getMinutes() + 1 ];
-  var duskEnd = [ times.night.getHours(), times.night.getMinutes() ];
+//   var duskStart = [ times.sunset.getHours(), times.sunset.getMinutes() + 1 ];
+//   var duskEnd = [ times.night.getHours(), times.night.getMinutes() ];
 
-  var timeBlocks = [
-    { start: nightStart, end: nightEnd, class: "night" },
-    { start: dawnStart, end: dawnEnd, class: "dawn" },
-    { start: dayStart, end: dayEnd, class: "day" },
-    { start: duskStart, end: duskEnd, class: "dusk" }
-  ]
+//   var timeBlocks = [
+//     { start: nightStart, end: nightEnd, class: "night" },
+//     { start: dawnStart, end: dawnEnd, class: "dawn" },
+//     { start: dayStart, end: dayEnd, class: "day" },
+//     { start: duskStart, end: duskEnd, class: "dusk" }
+//   ]
 
-  // Start by looping through the objects in the timeBlocks array
-  for ( var i = 0; i < timeBlocks.length; i++ ) {
-    // Select the current timeBlock
-    var timeOfDay = timeBlocks[i];
-    // console.log("Time of Day: ", timeOfDay);
+//   // Start by looping through the objects in the timeBlocks array
+//   for ( var i = 0; i < timeBlocks.length; i++ ) {
+//     // Select the current timeBlock
+//     var timeOfDay = timeBlocks[i];
+//     // console.log("Time of Day: ", timeOfDay);
 
-    if ( isTimeBetween(timeOfDay.start, timeOfDay.end) ) {
-      $sky.addClass(timeOfDay.class);
-    }
-  }
+//     if ( isTimeBetween(timeOfDay.start, timeOfDay.end) ) {
+//       $sky.addClass(timeOfDay.class);
+//     }
+//   }
 
-  if ( $("header.sky").hasClass("night") ) {
-    $(".sky-stars-wrapper").css("display", "block");
-  } else {
-    $(".sky-stars-wrapper").css("display", "none");
-  }
+//   if ( $("header.sky").hasClass("night") ) {
+//     $(".sky-stars-wrapper").css("display", "block");
+//   } else {
+//     $(".sky-stars-wrapper").css("display", "none");
+//   }
   
-  if ( $("header.sky").hasClass("day") || $("header.sky").hasClass("dusk") ) {
-    $("#hotairballoon").css("display", "block");
-  } else {
-    $("#hotairballoon").css("display", "none");
-  }
+//   if ( $("header.sky").hasClass("day") || $("header.sky").hasClass("dusk") ) {
+//     $("#hotairballoon").css("display", "block");
+//   } else {
+//     $("#hotairballoon").css("display", "none");
+//   }
 
-  var dateObject = new Date();
-  var month = dateObject.getMonth() + 1;
-  var day = dateObject.getDate();
-  var nowDate = dateObject.getFullYear() + "-" + (month < 10 ? '0' : '') + month + "-" + (day < 10 ? '0' : '') + day;
+//   var dateObject = new Date();
+//   var month = dateObject.getMonth() + 1;
+//   var day = dateObject.getDate();
+//   var nowDate = dateObject.getFullYear() + "-" + (month < 10 ? '0' : '') + month + "-" + (day < 10 ? '0' : '') + day;
 
-  var xmasStart = dateObject.getFullYear() + "-12-01";
-  var xmasEnd = dateObject.getFullYear() + "-12-27";
-  if (nowDate >= xmasStart && nowDate <= xmasEnd) {
-    $("#santahat").css("display", "block");
-  } else {
-    $("#santahat").css("display", "none");
-  }
+//   var xmasStart = dateObject.getFullYear() + "-12-01";
+//   var xmasEnd = dateObject.getFullYear() + "-12-27";
+//   if (nowDate >= xmasStart && nowDate <= xmasEnd) {
+//     $("#santahat").css("display", "block");
+//   } else {
+//     $("#santahat").css("display", "none");
+//   }
 
-  var halloweenStart = dateObject.getFullYear() + "-10-07";
-  var halloweenEnd = dateObject.getFullYear() + "-10-31";
-  if (nowDate >= halloweenStart && nowDate <= halloweenEnd) {
-    $("#ghosty").css("display", "block");
-  } else {
-    $("#ghosty").css("display", "none");
-  }
+//   var halloweenStart = dateObject.getFullYear() + "-10-07";
+//   var halloweenEnd = dateObject.getFullYear() + "-10-31";
+//   if (nowDate >= halloweenStart && nowDate <= halloweenEnd) {
+//     $("#ghosty").css("display", "block");
+//   } else {
+//     $("#ghosty").css("display", "none");
+//   }
 
-}
+// }
 
-// ***************** Widget Most Loved
-$.getJSON("/index.json", function( data ) {
-  var items = [];
-  var counter = 0;
+// // ***************** Widget Most Loved
+// $.getJSON("/index.json", function( data ) {
+//   var items = [];
+//   var counter = 0;
 
-  data.sort(compare);
+//   data.sort(compare);
 
-  $.each(data, function(key, val) {
+//   $.each(data, function(key, val) {
     
-    if (counter == 5) {
-      return false
-    }
+//     if (counter == 5) {
+//       return false
+//     }
 
-    if (val.commentsCount == 0) {
-      return false
-    }
+//     if (val.commentsCount == 0) {
+//       return false
+//     }
 
-    items.push("<li><span class='variable'><a href='" + val.permalink + "#comments' alt='" + val.title + "' title='" 
-      + val.title + "'><svg class='remix-small comments'><use xlink:href='/fonts/remixicon/remixicon.symbol.svg#question-answer-line'></use></svg>"
-      + val.commentsCount + "</a></span><span class='variable-number'><a href='" + val.permalink + "' alt='" 
-      + val.title + "' title='" + val.title + "'>" + val.title + "</a></span></li>" );
+//     items.push("<li><span class='variable'><a href='" + val.permalink + "#comments' alt='" + val.title + "' title='" 
+//       + val.title + "'><svg class='remix-small comments'><use xlink:href='/fonts/remixicon/remixicon.symbol.svg#question-answer-line'></use></svg>"
+//       + val.commentsCount + "</a></span><span class='variable-number'><a href='" + val.permalink + "' alt='" 
+//       + val.title + "' title='" + val.title + "'>" + val.title + "</a></span></li>" );
 
-    counter++;
-  });
+//     counter++;
+//   });
 
-  $("<ul/>", {
-    html: items.join("")
-  }).appendTo( ".widget.mostloved.box" );
-});
+//   $("<ul/>", {
+//     html: items.join("")
+//   }).appendTo( ".widget.mostloved.box" );
+// });
 
 
-function compare(a, b) {
-  if (a.commentsCount < b.commentsCount)
-    return 1;
-  if (a.commentsCount > b.commentsCount)
-    return -1;
-  return 0;
-}
+// function compare(a, b) {
+//   if (a.commentsCount < b.commentsCount)
+//     return 1;
+//   if (a.commentsCount > b.commentsCount)
+//     return -1;
+//   return 0;
+// }
 
-function isTimeBetween(startTimeAsArray, endTimeAsArray) {
-  // THANKS: https://stackoverflow.com/a/25958232
+// function isTimeBetween(startTimeAsArray, endTimeAsArray) {
+//   // THANKS: https://stackoverflow.com/a/25958232
 
-  var startTime = startTimeAsArray;
-  var endTime = endTimeAsArray;
+//   var startTime = startTimeAsArray;
+//   var endTime = endTimeAsArray;
 
-  // We've got the two start times as an array of hours/minutes values.
-  var dateObj = new Date(); 
-  // var now = [23, 0]; // For testing purpose, set now time here
-  var now = [dateObj.getHours(), dateObj.getMinutes()]; // Gets the current Hours/Minutes
+//   // We've got the two start times as an array of hours/minutes values.
+//   var dateObj = new Date(); 
+//   // var now = [23, 0]; // For testing purpose, set now time here
+//   var now = [dateObj.getHours(), dateObj.getMinutes()]; // Gets the current Hours/Minutes
 
-  // If startTime is bigger than endTime
-  if ( endTime[0] < startTime[0] && now[0] < startTime[0] ) {
-    startTime[0] -= 24; // This is something I came up with because I do a lot of math.
-  } else if ( startTime[0] > endTime[0] ) {
-    endTime[0] += 24;
-  }
+//   // If startTime is bigger than endTime
+//   if ( endTime[0] < startTime[0] && now[0] < startTime[0] ) {
+//     startTime[0] -= 24; // This is something I came up with because I do a lot of math.
+//   } else if ( startTime[0] > endTime[0] ) {
+//     endTime[0] += 24;
+//   }
 
-  // the time strings converted to a string format. Made comparisons easier.
-  var startString = to_hm_string(startTime);
-  var endString = to_hm_string(endTime);
-  var nowString = to_hm_string(now);
+//   // the time strings converted to a string format. Made comparisons easier.
+//   var startString = to_hm_string(startTime);
+//   var endString = to_hm_string(endTime);
+//   var nowString = to_hm_string(now);
 
-  // console.log("Now:", nowString, "Start:", startString, "End:", endString);
+//   // console.log("Now:", nowString, "Start:", startString, "End:", endString);
 
-  var status = (startString <= nowString && nowString <= endString) ? true : false;
-  // console.log(status);
-  return status;
-}
+//   var status = (startString <= nowString && nowString <= endString) ? true : false;
+//   // console.log(status);
+//   return status;
+// }
 
-// Numbers to String, if startTime bigger than endTime, set startTime to minus
-// startTime(18:18) endTime(06:26) = startTime(-06:18) endTime(06:26)
-function to_hm_string(timearr){ 
-  var hours = "";
-  var minutes = timearr[1];
+// // Numbers to String, if startTime bigger than endTime, set startTime to minus
+// // startTime(18:18) endTime(06:26) = startTime(-06:18) endTime(06:26)
+// function to_hm_string(timearr){ 
+//   var hours = "";
+//   var minutes = timearr[1];
 
-  if ( Math.abs(timearr[0]) < 10 ) {
-    hours = "0";
-  }
-  hours = ( timearr[0] < 0 ) ? "-" + hours + Math.abs(timearr[0]) : hours + timearr[0];
-  minutes = (minutes < 10 ? '0' : '') + minutes;
+//   if ( Math.abs(timearr[0]) < 10 ) {
+//     hours = "0";
+//   }
+//   hours = ( timearr[0] < 0 ) ? "-" + hours + Math.abs(timearr[0]) : hours + timearr[0];
+//   minutes = (minutes < 10 ? '0' : '') + minutes;
 
-  // console.log(hours + ":" + minutes);
-  return hours + ":" + minutes;
-}
+//   // console.log(hours + ":" + minutes);
+//   return hours + ":" + minutes;
+// }
