8df18f3ab0051faa9fe010455b51b9cd35b67d84
Angel Crawford <16148683+AngelCrawford@users.noreply.github.com>
2025-06-01 17:46:02 +0200

    Highlight of navbar for dropdown items

---
+++ assets/js/navbar.js
@@ -0,0 +1,24 @@
+// ***************** Navigation Scroll
+function navScroll() {
+    var theNavigation = $(".navbar");
+    stuck = "is-fixed-top";
+    theHeader = 300;
+  
+    console.log(theHeader);
+  
+    if ($(window).scrollTop() > theHeader) {
+      theNavigation.addClass(stuck);
+    } else {
+      theNavigation.removeClass(stuck);
+    }
+}
+$(window).on('scroll',function() { 
+    navScroll();
+});
+
+
+// ***************** Navigation
+// Check for click events on the navbar burger icon
+$(".button.toggle-sidebar").click(function() {
+    $("aside.sidebar").toggleClass("is-opened");
+});
